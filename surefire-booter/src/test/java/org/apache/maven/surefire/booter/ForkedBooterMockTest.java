23d17cf0e4634ca52c159a49292bf2e562fc1c48
tibordigana <tibordigana@apache.org>
2020-02-15 10:34:31 +0100

    covered ForkedBooter and extensions SPI

--- surefire-booter/src/test/java/org/apache/maven/surefire/booter/ForkedBooterMockTest.java
+++ surefire-booter/src/test/java/org/apache/maven/surefire/booter/ForkedBooterMockTest.java
@@ -19,33 +19,51 @@ package org.apache.maven.surefire.booter;
  * under the License.
  */
 
+import org.apache.maven.surefire.booter.spi.LegacyMasterProcessChannelDecoder;
 import org.apache.maven.surefire.booter.spi.LegacyMasterProcessChannelEncoder;
+import org.apache.maven.surefire.booter.spi.LegacyMasterProcessChannelProcessorFactory;
+import org.apache.maven.surefire.booter.spi.SurefireMasterProcessChannelProcessorFactory;
+import org.apache.maven.surefire.providerapi.MasterProcessChannelDecoder;
+import org.apache.maven.surefire.providerapi.MasterProcessChannelEncoder;
 import org.apache.maven.surefire.report.StackTraceWriter;
+import org.apache.maven.surefire.spi.MasterProcessChannelProcessorFactory;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.nio.channels.ServerSocketChannel;
+
+import static java.net.StandardSocketOptions.SO_KEEPALIVE;
+import static java.net.StandardSocketOptions.SO_REUSEADDR;
+import static java.net.StandardSocketOptions.TCP_NODELAY;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.powermock.api.mockito.PowerMockito.doCallRealMethod;
 import static org.powermock.api.mockito.PowerMockito.doNothing;
 import static org.powermock.api.mockito.PowerMockito.doThrow;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyNoMoreInteractions;
 import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.reflect.Whitebox.invokeMethod;
 import static org.powermock.reflect.Whitebox.setInternalState;
-import static org.powermock.utils.JavaVersion.JAVA_RECENT;
-import static org.powermock.utils.JavaVersion.JAVA_12;
 
 /**
  * PowerMock tests for {@link ForkedBooter}.
@@ -55,11 +73,17 @@ import static org.powermock.utils.JavaVersion.JAVA_12;
 @PowerMockIgnore( { "org.jacoco.agent.rt.*", "com.vladium.emma.rt.*" } )
 public class ForkedBooterMockTest
 {
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
     @Mock
     private PpidChecker pluginProcessChecker;
 
     @Mock
     private ForkedBooter booter;
+
+    @Mock
+    private MasterProcessChannelProcessorFactory channelProcessorFactory;
 
     @Mock
     private LegacyMasterProcessChannelEncoder eventChannel;
@@ -94,7 +118,7 @@ public class ForkedBooterMockTest
     @Test
     public void testMain() throws Exception
     {
-        PowerMockito.mockStatic( ForkedBooter.class );
+        mockStatic( ForkedBooter.class );
 
         doCallRealMethod()
                 .when( ForkedBooter.class, "run", capturedBooter.capture(), capturedArgs.capture() );
@@ -129,14 +153,7 @@ public class ForkedBooterMockTest
     @Test
     public void testMainWithError() throws Exception
     {
-        //does not work in PowerMock
-        //assumeFalse( JAVA_RECENT.atLeast( JAVA_12 ) );
-        if ( JAVA_RECENT.atLeast( JAVA_12 ) )
-        {
-            return;
-        }
-
-        PowerMockito.mockStatic( ForkedBooter.class );
+        mockStatic( ForkedBooter.class );
 
         doCallRealMethod()
                 .when( ForkedBooter.class, "run", any( ForkedBooter.class ), any( String[].class ) );
@@ -148,7 +165,6 @@ public class ForkedBooterMockTest
                 .when( booter, "setupBooter",
                         any( String.class ), any( String.class ), any( String.class ), any( String.class ) );
 
-        // broken in PowerMock JDK 12
         setInternalState( booter, "eventChannel", eventChannel );
 
         String[] args = new String[]{ "/", "dump", "surefire.properties", "surefire-effective.properties" };
@@ -179,5 +195,160 @@ public class ForkedBooterMockTest
                 .invoke( "exit1" );
 
         verifyNoMoreInteractions( booter );
+    }
+
+    @Test
+    public void shouldNotCloseChannelProcessorFactory() throws Exception
+    {
+        setInternalState( booter, "channelProcessorFactory", (MasterProcessChannelProcessorFactory) null );
+
+        doCallRealMethod()
+            .when( booter, "closeForkChannel" );
+
+        invokeMethod( booter, "closeForkChannel" );
+
+        verifyZeroInteractions( channelProcessorFactory );
+    }
+
+    @Test
+    public void shouldCloseChannelProcessorFactory() throws Exception
+    {
+        setInternalState( booter, "channelProcessorFactory", channelProcessorFactory );
+
+        doCallRealMethod()
+            .when( booter, "closeForkChannel" );
+
+        invokeMethod( booter, "closeForkChannel" );
+
+        verify( channelProcessorFactory, times( 1 ) )
+            .close();
+        verifyNoMoreInteractions( channelProcessorFactory );
+    }
+
+    @Test
+    public void shouldFailOnCloseChannelProcessorFactory() throws Exception
+    {
+        setInternalState( booter, "channelProcessorFactory", channelProcessorFactory );
+
+        doThrow( new IOException() )
+            .when( channelProcessorFactory )
+            .close();
+
+        doCallRealMethod()
+            .when( booter, "closeForkChannel" );
+
+        invokeMethod( booter, "closeForkChannel" );
+
+        verify( channelProcessorFactory, times( 1 ) )
+            .close();
+        verifyNoMoreInteractions( channelProcessorFactory );
+    }
+
+    @Test
+    public void shouldLookupLegacyDecoderFactory() throws Exception
+    {
+        mockStatic( ForkedBooter.class );
+
+        doCallRealMethod()
+            .when( ForkedBooter.class, "lookupDecoderFactory", anyString() );
+
+        try ( final MasterProcessChannelProcessorFactory factory =
+                  invokeMethod( ForkedBooter.class, "lookupDecoderFactory", "pipe://3" ) )
+        {
+            assertThat( factory ).isInstanceOf( LegacyMasterProcessChannelProcessorFactory.class );
+
+            assertThat( factory.canUse( "pipe://3" ) ).isTrue();
+
+            assertThat( factory.canUse( "-- whatever --" ) ).isFalse();
+
+            errorCollector.checkThrows( MalformedURLException.class, new ThrowingRunnable()
+            {
+                @Override
+                public void run() throws Throwable
+                {
+                    factory.connect( "tcp://localhost:123" );
+                    fail();
+                }
+            } );
+
+            factory.connect( "pipe://3" );
+
+            MasterProcessChannelDecoder decoder = factory.createDecoder();
+            assertThat( decoder ).isInstanceOf( LegacyMasterProcessChannelDecoder.class );
+            MasterProcessChannelEncoder encoder = factory.createEncoder();
+            assertThat( encoder ).isInstanceOf( LegacyMasterProcessChannelEncoder.class );
+        }
+    }
+
+    @Test
+    public void shouldLookupSurefireDecoderFactory() throws Exception
+    {
+        mockStatic( ForkedBooter.class );
+
+        doCallRealMethod()
+            .when( ForkedBooter.class, "lookupDecoderFactory", anyString() );
+
+        try ( ServerSocketChannel server = ServerSocketChannel.open() )
+        {
+            if ( server.supportedOptions().contains( SO_REUSEADDR ) )
+            {
+                server.setOption( SO_REUSEADDR, true );
+            }
+
+            if ( server.supportedOptions().contains( TCP_NODELAY ) )
+            {
+                server.setOption( TCP_NODELAY, true );
+            }
+
+            if ( server.supportedOptions().contains( SO_KEEPALIVE ) )
+            {
+                server.setOption( SO_KEEPALIVE, true );
+            }
+
+            server.bind( new InetSocketAddress( 0 ) );
+            int serverPort = ( (InetSocketAddress) server.getLocalAddress() ).getPort();
+
+            try ( MasterProcessChannelProcessorFactory factory =
+                     invokeMethod( ForkedBooter.class, "lookupDecoderFactory", "tcp://127.0.0.1:" + serverPort ) )
+            {
+                assertThat( factory )
+                    .isInstanceOf( SurefireMasterProcessChannelProcessorFactory.class );
+
+                assertThat( factory.canUse( "tcp://127.0.0.1:" + serverPort ) )
+                    .isTrue();
+
+                assertThat( factory.canUse( "-- whatever --" ) )
+                    .isFalse();
+
+                errorCollector.checkThrows( MalformedURLException.class, new ThrowingRunnable()
+                {
+                    @Override
+                    public void run() throws Throwable
+                    {
+                        factory.connect( "pipe://1" );
+                        fail();
+                    }
+                } );
+
+                errorCollector.checkThrows( IOException.class, new ThrowingRunnable()
+                {
+                    @Override
+                    public void run() throws Throwable
+                    {
+                        factory.connect( "tcp://localhost:123\u0000\u0000\u0000" );
+                        fail();
+                    }
+                } );
+
+                factory.connect( "tcp://127.0.0.1:" + serverPort );
+
+                MasterProcessChannelDecoder decoder = factory.createDecoder();
+                assertThat( decoder )
+                    .isInstanceOf( LegacyMasterProcessChannelDecoder.class );
+                MasterProcessChannelEncoder encoder = factory.createEncoder();
+                assertThat( encoder )
+                    .isInstanceOf( LegacyMasterProcessChannelEncoder.class );
+            }
+        }
     }
 }
